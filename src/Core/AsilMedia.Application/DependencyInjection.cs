66954138fae4c949c6f96516e0c064a47ca6ecd6
SardorSohinazarov <sardorstudent0618gmail.com>
2024-02-23 10:33:47 +0500

    DI implemented

---
+++ src/Core/AsilMedia.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using AsilMedia.Application.Services.Actors;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AsilMedia.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IActorService, ActorService>();
+            //davom etamiz
+
+            return services;
+        }
+    }
+}
