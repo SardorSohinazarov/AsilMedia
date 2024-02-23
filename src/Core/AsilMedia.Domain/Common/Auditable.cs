00d21915b70e82551673aa8bb581c5d60454c49c
SardorSohinazarov <sardorstudent0618gmail.com>
2024-02-23 10:31:36 +0500

    common entities implemented

---
+++ src/Core/AsilMedia.Domain/Common/Auditable.cs
@@ -0,0 +1,8 @@
+﻿namespace AsilMedia.Domain.Common
+{
+    public abstract class Auditable : BaseEntity
+    {
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ src/Core/AsilMedia.Domain/Common/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace AsilMedia.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public long Id { get; set; }
+    }
+}
