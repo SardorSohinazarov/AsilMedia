6c0b1bf1d9da9f2955206119ce3c6c709b11f87e
SardorSohinazarov <sardorstudent0618gmail.com>
2024-02-25 18:42:22 +0500

    Exceptions added

---
+++ src/Core/AsilMedia.Domain/Exceptions/Films/FilmNotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace AsilMedia.Domain.Exceptions.Films
+{
+    public class FilmNotFoundException : Exception
+    {
+        public FilmNotFoundException() : base("Film not found.") { }
+    }
+}
