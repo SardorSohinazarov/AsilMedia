d4e0b6ddd8a80bdf608aa07944af2baf70e5c9ef
SardorSohinazarov <sardorstudent0618gmail.com>
2024-02-25 18:41:13 +0500

    Film Service Implemented

--- src/Core/AsilMedia.Application/Services/Films/FilmService.cs
+++ src/Core/AsilMedia.Application/Services/Films/FilmService.cs
@@ -1,6 +1,51 @@
-﻿namespace AsilMedia.Application.Services.Films
+﻿using AsilMedia.Application.Abstractions.Repositories;
+using AsilMedia.Application.DataTransferObjects;
+using AsilMedia.Domain.Entities;
+
+namespace AsilMedia.Application.Services.Films
 {
     public class FilmService : IFilmService
     {
+        private readonly IFilmRepository _filmRepository;
+
+        public FilmService(IFilmRepository filmRepository)
+            => _filmRepository = filmRepository;
+
+        public async Task<Film> InsertAsync(FilmDTO filmDTO)
+        {
+            var film = new Film()
+            {
+                Title = filmDTO.Title,
+                AgeRestriction = filmDTO.AgeRestriction,
+
+                ///...
+            };
+
+            film = await _filmRepository.InsertAsync(film);
+
+            return film;
+        }
+
+        public async Task<List<Film>> SelectAllAsync()
+            => await _filmRepository.SelectAllAsync();
+
+        public async Task<Film> SelectByIdAsync(long id)
+            => await _filmRepository.SelectByIdAsync(id);
+
+        public async Task<Film> UpdateAsync(FilmDTO filmDTO, long id)
+        {
+            var film = new Film()
+            {
+                Title = filmDTO.Title,
+                AgeRestriction = filmDTO.AgeRestriction,
+
+                ///...
+            };
+
+            return await _filmRepository.UpdateAsync(film, id);
+        }
+
+        public async Task<Film> DeleteAsync(long id)
+            => await _filmRepository.DeleteAsync(id);
     }
 }
--- src/Core/AsilMedia.Application/Services/Films/IFilmService.cs
+++ src/Core/AsilMedia.Application/Services/Films/IFilmService.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AsilMedia.Application.DataTransferObjects;
+using AsilMedia.Domain.Entities;
 
 namespace AsilMedia.Application.Services.Films
 {
     public interface IFilmService
     {
+        public Task<Film> InsertAsync(FilmDTO filmDTO);
+        public Task<Film> SelectByIdAsync(long id);
+        public Task<List<Film>> SelectAllAsync();
+        public Task<Film> UpdateAsync(FilmDTO filmDTO, long id);
+        public Task<Film> DeleteAsync(long id);
     }
 }
