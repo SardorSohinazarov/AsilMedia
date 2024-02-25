b80d666c149d57423bd680b0ceeeeadb2b93bbe1
SardorSohinazarov <sardorstudent0618gmail.com>
2024-02-25 18:45:35 +0500

    DI extension added

---
+++ src/External/AsilMedia.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using AsilMedia.Application.Abstractions.Repositories;
+using AsilMedia.Infrastructure1.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AsilMedia.Infrastructure1
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("Postgress")));
+
+            services.AddScoped<IFilmRepository, FilmRepository>();
+
+            return services;
+        }
+    }
+}
