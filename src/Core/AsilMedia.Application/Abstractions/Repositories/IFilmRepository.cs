b9a275062346984fcb04dd6e5a7bfcfa41937885
SardorSohinazarov <sardorstudent0618gmail.com>
2024-02-25 18:40:35 +0500

    Repository Abstraction Implemented

---
+++ src/Core/AsilMedia.Application/Abstractions/Repositories/IFilmRepository.cs
@@ -0,0 +1,13 @@
+﻿using AsilMedia.Domain.Entities;
+
+namespace AsilMedia.Application.Abstractions.Repositories
+{
+    public interface IFilmRepository
+    {
+        public Task<Film> InsertAsync(Film film);
+        public Task<Film> SelectByIdAsync(long id);
+        public Task<List<Film>> SelectAllAsync();
+        public Task<Film> UpdateAsync(Film film, long id);
+        public Task<Film> DeleteAsync(long id);
+    }
+}
