4b44fe77642ed2ead9b748613b8a113be21df08a
SardorSohinazarov <sardorstudent0618gmail.com>
2024-02-25 18:40:53 +0500

    DTOs implemented

---
+++ src/Core/AsilMedia.Application/DataTransferObjects/FilmDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace AsilMedia.Application.DataTransferObjects
+{
+    public class FilmDTO
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public long FilmMakerId { get; set; }
+        public int AgeRestriction { get; set; }
+        public int PublishedYear { get; set; }
+        public string PhotoPath { get; set; }
+        public string VideoPath { get; set; }
+
+        public List<long> Genres { get; set; }
+        public List<long> Actors { get; set; }
+    }
+}
