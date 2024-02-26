b57ac91758d19d951e80a0d16aa349e3271c6032
SardorSohinazarov <sardorstudent0618gmail.com>
2024-02-26 09:08:01 +0500

    Genre Services implemented

--- src/Core/AsilMedia.Application/Abstractions/Repositories/IGenreRepository.cs
+++ src/Core/AsilMedia.Application/Abstractions/Repositories/IGenreRepository.cs
@@ -1,18 +1,13 @@
 ﻿using AsilMedia.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AsilMedia.Application.Abstractions.Repositories
 {
     public interface IGenreRepository
     {
-        public Task<Genre> InsertAsync(Genre gen);
+        public Task<Genre> InsertAsync(Genre genre);
         public Task<Genre> SelectByIdAsync(long id);
         public Task<List<Genre>> SelectAllAsync();
-        public Task<Genre> UpdateAsync(Genre gen, long id);
+        public Task<Genre> UpdateAsync(Genre genre, long id);
         public Task<Genre> DeleteAsync(long id);
     }
 }
--- src/Core/AsilMedia.Application/Services/Genres/GenreService.cs
+++ src/Core/AsilMedia.Application/Services/Genres/GenreService.cs
@@ -1,6 +1,51 @@
-﻿namespace AsilMedia.Application.Services.Genres
+﻿using AsilMedia.Application.Abstractions.Repositories;
+using AsilMedia.Application.DataTransferObjects;
+using AsilMedia.Domain.Entities;
+
+namespace AsilMedia.Application.Services.Genres
 {
     public class GenreService : IGenreService
     {
+        private readonly IGenreRepository _genreRepository;
+
+        public GenreService(IGenreRepository genreRepository)
+            => _genreRepository = genreRepository;
+
+        public async Task<Genre> InsertAsync(GenreDTO genreDTO)
+        {
+            var genre = new Genre()
+            {
+                Name = genreDTO.Name
+            };
+
+            genre = await _genreRepository.InsertAsync(genre);
+
+            return genre;
+        }
+
+        public async Task<List<Genre>> SelectAllAsync()
+            => await _genreRepository.SelectAllAsync();
+
+        public async Task<Genre> SelectByIdAsync(long id)
+            => await _genreRepository.SelectByIdAsync(id);
+
+        public async Task<Genre> UpdateAsync(GenreDTO genreDTO, long id)
+        {
+            var genre = new Genre()
+            {
+                Name = genreDTO.Name
+            };
+
+            var gerne = await _genreRepository.UpdateAsync(genre, id);
+
+            return genre;
+        }
+
+        public async Task<Genre> DeleteAsync(long id)
+        {
+            var genre = await _genreRepository.DeleteAsync(id);
+
+            return genre;
+        }
     }
 }
