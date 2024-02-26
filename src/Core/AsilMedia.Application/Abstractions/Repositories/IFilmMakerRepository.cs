41a9d2e56ce93aff8539e868706e5b4e4a45f67f
tursunboev <hayotillotursunboev7@gmail.com>
2024-02-26 09:49:11 +0500

    filmMakerRepository ,FilmMakerDTO , FilmMakerNotFoundExeption done

---
+++ src/Core/AsilMedia.Application/Abstractions/Repositories/IFilmMakerRepository.cs
@@ -0,0 +1,18 @@
+﻿using AsilMedia.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsilMedia.Application.Abstractions.Repositories
+{
+    public interface IFilmMakerRepository
+    {
+        public Task<FilmMaker> InsertAsync(FilmMaker filmMaker);
+        public Task<FilmMaker> SelectByIdAsync(long id);
+        public Task<List<FilmMaker>> SelectAllAsync();
+        public Task<FilmMaker> UpdateAsync(FilmMaker filmMaker, long id);
+        public Task<FilmMaker> DeleteAsync(long id);
+    }
+}
