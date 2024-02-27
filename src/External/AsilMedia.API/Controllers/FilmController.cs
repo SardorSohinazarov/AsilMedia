b59d1ce29fc8a5972ae769f3ef484e74ce9568ce
Tohirjon-Odilov <shahanshoh819@gmail.com>
2024-02-27 08:46:47 +0500

    fixed controller

--- src/External/AsilMedia.API/Controllers/FilmController.cs
+++ src/External/AsilMedia.API/Controllers/FilmController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AsilMedia.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/action")]
     [ApiController]
     public class FilmController : ControllerBase
     {
