c412d5edfd6bc645d3386ea4bf4903c34d816488
SardorSohinazarov <sardorstudent0618gmail.com>
2024-03-01 00:07:24 +0500

    Role va User entity

---
+++ src/Core/AsilMedia.Domain/Entities/Role.cs
@@ -0,0 +1,11 @@
+﻿using AsilMedia.Domain.Common;
+
+namespace AsilMedia.Domain.Entities
+{
+    public class Role : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public List<User> Users { get; set; }
+    }
+}
---
+++ src/Core/AsilMedia.Domain/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using AsilMedia.Domain.Common;
+
+namespace AsilMedia.Domain.Entities
+{
+    public class User : Auditable
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime ExpiredDate { get; set; }
+        public long RoleId { get; set; }
+
+        public Role Role { get; set; }
+    }
+}
