7c65a02ac7b81d2ecc716d65ae086126106dbd9b
SardorSohinazarov <sardorstudent0618gmail.com>
2024-03-01 00:04:38 +0500

    DB Abstrations

---
+++ src/Core/AsilMedia.Application/Abstractions/IApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using AsilMedia.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AsilMedia.Application.Abstractions
+{
+    public interface IApplicationDbContext
+    {
+        public DbSet<Film> Films { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<FilmMaker> FilmMakers { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+        public ValueTask<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/Core/AsilMedia.Application/Abstractions/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using AsilMedia.Domain.Entities;
+
+namespace AsilMedia.Application.Abstractions.Repositories
+{
+    public interface IUserRepository
+    {
+        public Task<User> InsertAsync(User user);
+        public Task<User> SelectByIdAsync(long id);
+        public Task<List<User>> SelectAllAsync();
+        public Task<User> UpdateAsync(User user, long id);
+        public Task<User> DeleteAsync(long id);
+    }
+}
