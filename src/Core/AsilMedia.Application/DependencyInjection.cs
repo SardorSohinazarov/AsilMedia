582449284b4fb4dd19a3b5593245b3131b6a837d
SardorSohinazarov <sardorstudent0618gmail.com>
2024-03-01 00:07:54 +0500

    Application DI Services

--- src/Core/AsilMedia.Application/DependencyInjection.cs
+++ src/Core/AsilMedia.Application/DependencyInjection.cs
@@ -1,19 +1,26 @@
-﻿using AsilMedia.Application.Services.Actors;
+﻿using AsilMedia.Application.Halpers.JWTServices;
+using AsilMedia.Application.Services.Actors;
 using AsilMedia.Application.Services.FilmMakers;
 using AsilMedia.Application.Services.Films;
 using AsilMedia.Application.Services.Genres;
+using AsilMedia.Application.Services.Roles;
+using AsilMedia.Application.Services.Users;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AsilMedia.Application
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddScoped<IGenreService, GenreService>();
             services.AddScoped<IFilmService, FilmService>();
             services.AddScoped<IFilmMakerService, FilmMakerService>();
             services.AddScoped<IActorService, ActorService>();
+            services.AddScoped<IRoleService, RoleService>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IJWTService, JWTService>();
 
             return services;
         }
