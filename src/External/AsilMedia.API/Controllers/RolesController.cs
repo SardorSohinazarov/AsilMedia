fd29369e57f4d1acc915b0a15e9d8241c3d564b7
SardorSohinazarov <sardorstudent0618gmail.com>
2024-03-01 00:13:09 +0500

    Controllers

---
+++ src/External/AsilMedia.API/Controllers/RolesController.cs
@@ -0,0 +1,29 @@
+﻿using AsilMedia.Application.Services.Roles;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AsilMedia.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        private readonly IRoleService _roleService;
+
+        public RolesController(IRoleService roleService)
+            => _roleService = roleService;
+
+        [HttpPost]
+        public async Task<IActionResult> PostRoleAsync(string name)
+        {
+            var role = await _roleService.InsertAsync(name);
+
+            return Ok(role);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllRoleAsync()
+        {
+            return Ok(await _roleService.SelectAllAsync());
+        }
+    }
+}
