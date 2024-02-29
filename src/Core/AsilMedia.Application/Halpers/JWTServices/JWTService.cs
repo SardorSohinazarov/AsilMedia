c8cac3e4e57377f967e823f6ab3b344b786674cc
SardorSohinazarov <sardorstudent0618gmail.com>
2024-03-01 00:05:03 +0500

    JWT Service

---
+++ src/Core/AsilMedia.Application/Halpers/JWTServices/JWTService.cs
@@ -0,0 +1,42 @@
+﻿using AsilMedia.Domain.Entities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AsilMedia.Application.Halpers.JWTServices
+{
+    public class JWTService : IJWTService
+    {
+        private readonly IConfiguration _configuration;
+
+        public JWTService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateAccessToken(User user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var exDate = Convert.ToInt32(_configuration["JWT:ExpireDate"] ?? "10");
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Role, user.Role.Name),
+                new Claim("Sardor", "Sohinazarov"),
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JWT:ValidIssuer"],
+                audience: _configuration["JWT:ValidAudience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(exDate),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
