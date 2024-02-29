ad5d92b6aca3366440efdfa6a92a01d38c489d57
SardorSohinazarov <sardorstudent0618gmail.com>
2024-03-01 00:04:51 +0500

    DTOs

---
+++ src/Core/AsilMedia.Application/DataTransferObjects/UserDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace AsilMedia.Application.DataTransferObjects
+{
+    public class UserDTO
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime ExpiredDate { get; set; } = DateTime.UtcNow;
+        public long RoleId { get; set; }
+    }
+}
