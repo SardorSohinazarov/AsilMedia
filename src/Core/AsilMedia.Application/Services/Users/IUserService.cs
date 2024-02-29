ded6c0a5ef2f880318b7c34e34510ef6a83d28a3
SardorSohinazarov <sardorstudent0618gmail.com>
2024-03-01 00:06:48 +0500

    User va Role Services

---
+++ src/Core/AsilMedia.Application/Services/Users/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using AsilMedia.Application.DataTransferObjects;
+using AsilMedia.Domain.Entities;
+
+namespace AsilMedia.Application.Services.Users
+{
+    public interface IUserService
+    {
+        public Task<string> InsertAsync(UserDTO userDTO);
+        public Task<User> SelectByIdAsync(long id);
+        public Task<List<User>> SelectAllAsync();
+        public Task<User> UpdateAsync(UserDTO userDTO, long id);
+        public Task<User> DeleteAsync(long id);
+    }
+}
